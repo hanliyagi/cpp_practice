941ec044e8f25663c9e5897450b05276412e0a21
hanliyagi <hanliyagi1@gmail.com>
2023-11-21 23:49:00 +0900

    labtest11

--- labtest.cpp
+++ labtest.cpp
@@ -1,2 +1,21 @@
 #include <iostream>
+#include <vector>
 
+using namespace std;
+
+class BigUnsigned {
+	vector<int> vec;
+	string str;
+	unsigned int num, size;
+public:
+	BigUnsigned() {
+		vec = { 0 };
+	}
+	BigUnsigned(unsigned int num) {
+
+	}
+};
+
+int main() {
+
+}
